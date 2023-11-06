970328c544a35f5c433793c68336660b0dae74e0
Ahmad Anas <102130991+iamAnas-zzx@users.noreply.github.com>
2023-11-06 18:19:08 +0530

    Third commit

--- routes/admin.js
+++ routes/admin.js
@@ -11,9 +11,9 @@ router.use(express.urlencoded({ extended: true }));
 router.use(methodOverride('_method'));
 
 //session middleware
-const requireLoginAdmin = (req, res , next) => { 
-    if(!req.session.admin_id){
-        req.flash('fail' , 'Sign In fisrt!');
+const requireLoginAdmin = (req, res, next) => {
+    if (!req.session.admin_id) {
+        req.flash('fail', 'Sign In fisrt!');
         return res.redirect('/admin/login');
     }
     next();
@@ -22,7 +22,11 @@ const requireLoginAdmin = (req, res , next) => {
 
 //Login Portion I have to do it in seperate
 router.get('/login', (req, res) => {
-    res.render('admin/login' ,  { messages: req.flash('fail') });
+    if(req.session.admin_id){
+        req.flash('logged', 'Already Loged In!');
+        return res.redirect('/admin/dashboard')
+    }
+    res.render('admin/login', { messages: req.flash('fail') });
 })
 
 //admin/dashboard part -
@@ -51,9 +55,9 @@ router.post('/dashboard', async (req, res) => {
 })
 
 
-router.get('/dashboard', requireLoginAdmin ,async (req, res) => {
+router.get('/dashboard', requireLoginAdmin, async (req, res) => {
     const data = await Admin.findOne({ University: "Madan Mohan Malaviya" }).populate('Faculty', 'name');
-    res.render('admin/dashboard', { data });
+    res.render('admin/dashboard', {messages : req.flash('logged'), data });
 })
 
 
@@ -63,7 +67,7 @@ router.get('/branch', requireLoginAdmin, async (req, res) => {
     const branchList = await Branch.find({});
     const showEditBox = false;
 
-    res.render('admin/branch', { branchList,  showEditBox });
+    res.render('admin/branch', { branchList, showEditBox });
 })
 
 
@@ -76,13 +80,13 @@ router.get('/branch/:id', requireLoginAdmin, async (req, res) => {
     res.render('admin/branch', { branchList, subjectsList, branch, showEditBox });
 })
 
-router.put('/branch/:id', requireLoginAdmin ,async (req, res) => {
+router.put('/branch/:id', requireLoginAdmin, async (req, res) => {
     try {
         const { id } = req.params;
         const newBranchName = req.body.name;
         const branch = await Branch.findById(id);
         //storing previous subjects name
-        const oldSubjectNames = branch.Subjects; 
+        const oldSubjectNames = branch.Subjects;
         //updating branch name 
         branch.name = newBranchName;
         //storing new subjects name
@@ -102,25 +106,25 @@ router.put('/branch/:id', requireLoginAdmin ,async (req, res) => {
 
         }
         //updating subject names in beanch BD
-        console.log(branch.Subjects);
-        for(let i = 0 ; i < branch.Subjects.length ; i++){
+        
+        for (let i = 0; i < branch.Subjects.length; i++) {
             branch.Subjects[i] = subjectNamePairs[i].newName;
         }
-        
+
         //updating subject names in student DB
-        const studentData = await Student.find({ branch : branch._id });
-        studentData.forEach(async (student) =>{
-            for(let i = 0 ; i < student.Subjects.length ; i++){
+        const studentData = await Student.find({ branch: branch._id });
+        studentData.forEach(async (student) => {
+            for (let i = 0; i < student.Subjects.length; i++) {
                 student.Subjects[i].subjectName = subjectNamePairs[i].newName;
             }
             await student.save();
         })
 
         //updating subject names in teacher DB
-        const teacherData  = await Teacher.find({branch : branch._id});
-        
-        teacherData.forEach( async (teacher) => {
-            teacher.subject.forEach((subjects , index) => {
+        const teacherData = await Teacher.find({ branch: branch._id });
+
+        teacherData.forEach(async (teacher) => {
+            teacher.subject.forEach((subjects, index) => {
                 subjectNamePairs.forEach((pair) => {
                     if (subjects === pair.oldName) {
                         // Update the subject name
@@ -130,7 +134,7 @@ router.put('/branch/:id', requireLoginAdmin ,async (req, res) => {
             });
             await teacher.save();
         });
-        
+
         await branch.save();
 
         res.redirect('/admin/branch');
@@ -140,7 +144,7 @@ router.put('/branch/:id', requireLoginAdmin ,async (req, res) => {
 })
 
 
-router.post('/branch', requireLoginAdmin ,async (req, res) => {
+router.post('/branch', requireLoginAdmin, async (req, res) => {
     try {
         const subjectNames = req.body.hiddenSubjects.split(',').map(subjectName => subjectName.trim());
 
@@ -149,13 +153,13 @@ router.post('/branch', requireLoginAdmin ,async (req, res) => {
             name: req.body.name,
             Subjects: subjectNames
         });
-        console.log(newBranch);
+      
         await newBranch.save();
 
-        const Uni = await Admin.findOne({ University : 'Madan Mohan Malaviya'});
+        const Uni = await Admin.findOne({ University: 'Madan Mohan Malaviya' });
         Uni.Faculty.push(newBranch._id);
         await Uni.save();
-        console.log(Uni);
+        
         res.redirect('/admin/branch');
     }
     catch (e) {
@@ -164,31 +168,29 @@ router.post('/branch', requireLoginAdmin ,async (req, res) => {
 
 })
 
-router.delete('/branch/:id' , requireLoginAdmin , async ( req,res) => {
-    try{
+router.delete('/branch/:id', requireLoginAdmin, async (req, res) => {
+    try {
         const { id } = req.params;
-        
-        const branch = await Branch.findById(id);
-        const studentIDS = await Student.find({branch : id});
-        const teacherIDS = await Teacher.find({branch : id});
+        const studentIDS = await Student.find({ branch: id });
+        const teacherIDS = await Teacher.find({ branch: id });
         console.log(studentIDS);
         console.log(teacherIDS);
-        for(let s of studentIDS){
+        for (let s of studentIDS) {
             await Student.findByIdAndDelete(s._id);
         }
-        for(let t of teacherIDS){
-            await Student.findByIdAndDelete(t._id);
+        for (let t of teacherIDS) {
+            await Teacher.findByIdAndDelete(t._id);
         }
         await Branch.findByIdAndDelete(id);
         res.redirect('/admin/branch');
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 })
 
 
 //admin/teachers part -
-router.get('/teachers', requireLoginAdmin , async (req, res) => {
+router.get('/teachers', requireLoginAdmin, async (req, res) => {
     try {
         const branchList = await Branch.find({});
         res.render('admin/teachers', { branchList });
@@ -196,7 +198,7 @@ router.get('/teachers', requireLoginAdmin , async (req, res) => {
         console.log(e);
     }
 })
-router.get('/teachers-data', requireLoginAdmin , async (req, res) => {
+router.get('/teachers-data', requireLoginAdmin, async (req, res) => {
     try {
         const branchList = await Branch.find({});
         const branch = req.query.branch;
@@ -209,7 +211,7 @@ router.get('/teachers-data', requireLoginAdmin , async (req, res) => {
 
 })
 
-router.get('/teachers-edit/:id', requireLoginAdmin ,  async (req, res) => {
+router.get('/teachers-edit/:id', requireLoginAdmin, async (req, res) => {
     const { id } = req.params;
     const teacher = await Teacher.findById(id);
     const branchID = teacher.branch;
@@ -217,11 +219,11 @@ router.get('/teachers-edit/:id', requireLoginAdmin ,  async (req, res) => {
     const branchDetails = await Branch.findById(branchID);
     const theTeachersData = await Teacher.find({ branch: branchDetails._id });
 
-    res.render('admin/teachers-edit', { branchList, branchDetails, theTeachersData , teacher});
+    res.render('admin/teachers-edit', { branchList, branchDetails, theTeachersData, teacher });
 })
 
 
-router.post('/teachers-data', requireLoginAdmin , async (req, res) => {
+router.post('/teachers-data', requireLoginAdmin, async (req, res) => {
     try {
 
         const branch = req.body.hiddenBranch;
@@ -248,10 +250,10 @@ router.post('/teachers-data', requireLoginAdmin , async (req, res) => {
     }
 })
 
-router.put('/teachers-edit/:id' , requireLoginAdmin , async (req,res) =>{
-    try{
-        const {id} = req.params;
-        const teacher =await Teacher.findById(id);
+router.put('/teachers-edit/:id', requireLoginAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const teacher = await Teacher.findById(id);
         teacher.name = req.body.name;
         teacher.loginId = req.body.loginId;
         teacher.password = req.body.password;
@@ -259,20 +261,32 @@ router.put('/teachers-edit/:id' , requireLoginAdmin , async (req,res) =>{
         console.log(teacher);
         await teacher.save();
         res.redirect('/admin/teachers');
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 })
 
-router.delete('/teachers/:id' , requireLoginAdmin , async ( req,res) => { 
-    const {id} = req.params;
+router.delete('/teachers/:id', requireLoginAdmin, async (req, res) => {
+    const { id } = req.params;
+    const teacher = await Teacher.findById(id).populate('branch');
+    const branchId = teacher.branch._id;
     await Teacher.findByIdAndDelete(id);
+
+    await Branch.updateOne(
+        { _id: branchId },
+        { $pull: { teacher: id } }
+
+    );
+    await Admin.updateOne(
+        { _id: req.session.admin_id },
+        { $pull: { Teachers: id } }
+    );
     res.redirect('/admin/teachers');
 })
 
 
 //admin/student part -
-router.get('/students', requireLoginAdmin ,  async (req, res) => {
+router.get('/students', requireLoginAdmin, async (req, res) => {
     try {
         const branchList = await Branch.find({});
         const branch = req.query.branch;
@@ -281,7 +295,7 @@ router.get('/students', requireLoginAdmin ,  async (req, res) => {
             showData = true;
             const branchData = await Branch.findOne({ name: branch });
             const studentData = await Student.find({ branch: branchData._id });
-            res.render('admin/students', { branchList, branch, studentData, showData});
+            res.render('admin/students', { branchList, branch, studentData, showData });
         } else {
             res.render('admin/students', { branchList, showData });
         }
@@ -290,25 +304,26 @@ router.get('/students', requireLoginAdmin ,  async (req, res) => {
     }
 })
 
-router.get('/students-edit/:id', requireLoginAdmin , async (req, res) => {
+router.get('/students-edit/:id', requireLoginAdmin, async (req, res) => {
     try {
         const { id } = req.params;
         const student = await Student.findById(id);
         const branchid = student.branch;
         const branch = await Branch.findById(branchid);
         const branchList = await Branch.find({});
-        const studentData = await Student.find({branch : branchid});
+        const studentData = await Student.find({ branch: branchid });
 
-        res.render('admin/students-edit', { branchList, branch , studentData , student });
+        res.render('admin/students-edit', { branchList, branch, studentData, student });
 
     } catch (e) {
+
         console.log(e);
     }
 })
 
 
 
-router.post('/students', requireLoginAdmin ,async (req, res) => {
+router.post('/students', requireLoginAdmin, async (req, res) => {
 
     const branch = req.body.branch;
     const branches = await Branch.findOne({ name: branch });
@@ -335,7 +350,7 @@ router.post('/students', requireLoginAdmin ,async (req, res) => {
     await branches.save();
 
     //updating admin
-    const Uni = await Admin.findOne({University : 'Madan Mohan Malaviya'});
+    const Uni = await Admin.findOne({ University: 'Madan Mohan Malaviya' });
     Uni.Student.push(newStudent._id);
     await Uni.save();
     console.log(Uni);
@@ -343,64 +358,80 @@ router.post('/students', requireLoginAdmin ,async (req, res) => {
     res.redirect('/admin/students');
 })
 
-router.put('/students-edit/:id' , requireLoginAdmin , async (req,res) =>{
-    try{
+router.put('/students-edit/:id', requireLoginAdmin, async (req, res) => {
+    try {
         const { id } = req.params;
         const student = await Student.findById(id);
         const branchid = student.branch;
         const subjectNames = student.Subjects;
-       
+
         //Now updating
         student.name = req.body.name,
-        student.Subjects = subjectNames,
-        student.branch = branchid,
-        student.rollNumber = req.body.rollNumber,
-        student.address = req.body.address,
-        student.loginId = req.body.loginId,
-        student.password = req.body.password
+            student.Subjects = subjectNames,
+            student.branch = branchid,
+            student.rollNumber = req.body.rollNumber,
+            student.address = req.body.address,
+            student.loginId = req.body.loginId,
+            student.password = req.body.password
         await student.save();
 
         res.redirect('/admin/students');
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
-        
+
 })
 
-router.delete('/students/:id' , requireLoginAdmin , async ( req,res) => { 
-    const {id} = req.params;
-    await Student.findByIdAndDelete(id);
-    res.redirect('/admin/students');
+router.delete('/students/:id', requireLoginAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const student = await Student.findById(id);
+        const branchId = student.branch;
+        await Branch.updateOne(
+            { _id: branchId },
+            { $pull: { student: id } }
+
+        );
+        await Admin.updateOne(
+            { _id: req.session.admin_id },
+            { $pull: { Student: id } }
+        );
+        await Student.findByIdAndDelete(id);
+        res.redirect('/admin/students');
+    } catch (e) {
+        console.log(e);
+        res.send(e);
+    }
 })
 
 
 //admin/password part - 
-router.get('/password', requireLoginAdmin , (req, res) => {
-    res.render('admin/password' , { messages: req.flash('validate') });
+router.get('/password', requireLoginAdmin, (req, res) => {
+    res.render('admin/password', { messages: req.flash('validate') });
 })
 
-router.post('/password', requireLoginAdmin , async (req,res)=>{
+router.post('/password', requireLoginAdmin, async (req, res) => {
     try {
         const id = req.session.admin_id;
         const admin = await Admin.findById(id);
-        const currPass = req.body.currPass; 
+        const currPass = req.body.currPass;
         const newPass = req.body.newPass;
         const newPassAgain = req.body.newPassAgain;
-        if(!newPass || !currPass || !newPassAgain){
-            req.flash('validate' , 'Please fill passwords correctly.');
+        if (!newPass || !currPass || !newPassAgain) {
+            req.flash('validate', 'Please fill passwords correctly.');
             return res.redirect('/admin/password');
         }
-        if(newPass !== newPassAgain){
-            req.flash('validate' , 'New passwords do not match!');
+        if (newPass !== newPassAgain) {
+            req.flash('validate', 'New passwords do not match!');
             return res.redirect('/admin/password');
         }
-        if(currPass !== admin.password){
-            req.flash('validate' , 'Cuurrent passwords is invalid!');
+        if (currPass !== admin.password) {
+            req.flash('validate', 'Cuurrent passwords is invalid!');
             return res.redirect('/admin/password');
         }
         admin.password = newPass;
         await admin.save();
-        req.flash('validate', 'Password change succesfully')        
+        req.flash('validate', 'Password change succesfully')
         res.redirect('/admin/password');
     } catch (error) {
         console.log(error);
@@ -408,9 +439,9 @@ router.post('/password', requireLoginAdmin , async (req,res)=>{
 })
 
 //logout part -
-router.get('/logout', requireLoginAdmin , (req,res) => {
+router.get('/logout', requireLoginAdmin, (req, res) => {
     req.session.admin_id = null;
     res.redirect('/admin/login');
-} )
+})
 
 module.exports = router;
